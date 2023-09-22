ae132f6601de5ee8b4ae21795ef0d697c8923f6d
Akza-John <115224724+Akza-John@users.noreply.github.com>
2023-09-22 17:44:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello, World!!this is a updated statement"
+    return "Hello, World!!!!-akza"
 
 if __name__ == '__main__':
     app.run(debug=True)
